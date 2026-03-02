15c65a2f91fb7451d45b6b2c5c555a3811b00cf1
orsicn <orsicn@oregonstate.edu>
2026-03-02 10:49:25 -0800

    add code

--- turfpractice/js/main.js
+++ turfpractice/js/main.js
@@ -1,1 +1,22 @@
 // Add all scripts to the JS folder
+
+var map =  L.map('map').setView([15, -15], 2
+); 
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+console.log("Map1 initialized");
+
+
+
+
+
+fetch("/data/eqpacific.geojson")
+  .then(response => response.json())
+  .then(data => {
+    var vorP = turf.voronoi(data);
+
+    L.geoJSON(data).addTo(map);
+    })
